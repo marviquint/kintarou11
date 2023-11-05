0206bf54f5938637606753384383564f14993659
marviquint <marviquintb@gmail.com>
2023-11-05 21:53:21 +0800

    updates 11-05-23

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import './portfolio.css'
 import Valorant from '../../assets/Valorant.png'
 import ML from '../../assets/ML.png'
+import GI from '../../assets/GI.png'
 import Dota from '../../assets/Dota.png'
 import vidValo from '../../assets/Valorant.mp4'
 import vidML from '../../assets/ML.mp4';
 import vidDota from '../../assets/Dota.mp4';
+import vidGI from '../../assets/GI.mp4';
 
 
 const Portfolio = () => {
@@ -15,6 +17,14 @@ const Portfolio = () => {
       <h2>Game Highlights</h2>
 
       <div className="container portfolio__container">
+      <article className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={GI} alt=""/>
+            </div>
+        </article>
+        <div className="video__ML">
+         <video autoPlay loop muted src={vidGI} id="ML__video" />
+        </div>
         <article className="portfolio__item">
             <div className="portfolio__item-image">
               <img src={Dota} alt=""/>
